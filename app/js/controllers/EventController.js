f9c82c7c9e195ac41f1ff2185955b5c9af2bafb4
SergerGood <serzher@inbox.ru>
2016-02-26 12:15:05 +0300

    first custom service

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -1,7 +1,7 @@
 'use strict';
 
 eventsApp.controller('EventController',
-    function EventController($scope) {
+    function EventController($scope, evenData) {
 
         $scope.snippet = '<span style="color:red;">hi there</span>';
         $scope.boolValue = true;
@@ -10,43 +10,7 @@ eventsApp.controller('EventController',
         $scope.buttonDisabled = true;
         $scope.sortorder = 'name';
 
-        $scope.event = {
-            name: 'Angular Boot Camp',
-            date: 1359781015626,
-            time: '12:12 am',
-            location:{
-                address: 'Street',
-                city: 'Moscow',
-                province: 'moscow'
-            },
-            imageUrl:'/img/angularjs-logo.png',
-            sessions: [
-                {
-                    name: 'Directives Masterclass',
-                    creationName: 'Bob',
-                    duration: 1,
-                    level: 'Advanced',
-                    abstract: 'Tra ta ta',
-                    upVoteCount: 0
-                },
-                {
-                    name: 'Scopes for fan and profit',
-                    creationName: 'John',
-                    duration: 2,
-                    level: 'Introductory',
-                    abstract: 'Tra ta ta',
-                    upVoteCount: 0
-                },
-                {
-                    name: 'Well Behaved Controllers',
-                    creationName: 'Jane',
-                    duration: 4,
-                    level: 'Intermediate',
-                    abstract: 'Tra ta ta',
-                    upVoteCount: 0
-                }
-            ]
-        };
+        $scope.event = evenData.event;
 
         $scope.upVoteSession = function (session) {
             session.upVoteCount++;
