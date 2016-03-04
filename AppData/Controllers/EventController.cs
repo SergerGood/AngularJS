a3df54ad95aa75fdbc83e02b3c2b16403ee052ac
SergerGood <serzher@inbox.ru>
2016-03-04 17:24:07 +0300

    updating an IIS Server for JSON Requests

---
+++ AppData/Controllers/EventController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace AppData.Controllers
+{
+    public class EventController : ApiController
+    {
+        public JToken Get(string id = null)
+        {
+            var path = System.Web.Hosting.HostingEnvironment.MapPath("/");
+            var fullPath = Path.Combine(path, @"../app/data/event", id + ".json");
+            var text = System.IO.File.ReadAllText(fullPath);
+
+            return JObject.Parse(text);
+        }
+
+        public void Post(string id, JObject eventData)
+        {
+            var path = System.Web.Hosting.HostingEnvironment.MapPath("/");
+            var fullPath = Path.Combine(path, @"../app/data/event", id + ".json");
+
+            System.IO.File.WriteAllText(fullPath, eventData.ToString(Formatting.None));
+        }
+    }
+}
---
+++ AppData/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AppData")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AppData")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4adfe5e9-e7e4-445a-8cf2-5f4d7c5c97e1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
