ba6bbcdd5864664f586781d118645a25bcc07011
SergerGood <serzher@inbox.ru>
2016-05-27 12:22:11 +0300

    first angular route

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,12 @@
 'use strict';
 
-var eventsApp = angular.module('eventsApp', ['ngResource'])//'ngSanitize', 'ngCookies'
+var eventsApp = angular.module('eventsApp', ['ngResource', 'ngRoute'])//'ngSanitize', 'ngCookies'
+    .config(function ($routeProvider) {
+        $routeProvider.when('/newEvent',{
+            templateUrl:'templates/NewEvent.html',
+            controller: 'EditEventController'
+        })
+    })
     .factory('myCache', function ($cacheFactory) {
         return $cacheFactory('myCache',{capacity:3});
     });
