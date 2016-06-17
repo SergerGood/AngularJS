534ff65d072a37bfa4d70359730a1bc5fc5e34e2
SergerGood <serzher@inbox.ru>
2016-06-17 18:11:58 +0300

    testing service in angular

---
+++ test/unit/calendarHelperSpec.js
@@ -0,0 +1,12 @@
+'use strict';
+
+describe('calendarHelper', function () {
+
+    beforeEach(module("eventsApp"));
+
+    it('should return January when given a zero', inject(function (calendarHelper) {
+
+        expect(calendarHelper.getMonthName(0)).toBe('January');
+
+    }))
+});
