97e1e75eb9ec4654586ce220a134d3cc02635c0a
SergerGood <serzher@inbox.ru>
2016-07-21 18:16:36 +0300

    testing service with dependencies

--- app/js/services/userData.js
+++ app/js/services/userData.js
@@ -1,3 +1,21 @@
-/**
- * Created by Khlebnikov.Sergey on 21.07.2016.
- */
+'use strict';
+
+eventsApp.factory('userData',['userResource', function (userResource) {
+
+    return{
+        getUser: function(userName, callback){
+            return userResource.get({userName: userName}, function (user) {
+                if(callback)
+                    callback(user);
+            });
+        },
+        save: function (user) {
+            userResource.save(user);
+        },
+        users: function () {
+            return userResource.queryAll(function (users) {
+                return users;
+            });
+        }
+    };
+}]);
--- test/unit/userDataSpec.js
+++ test/unit/userDataSpec.js
@@ -1,3 +1,44 @@
-/**
- * Created by Khlebnikov.Sergey on 21.07.2016.
- */
+'use strict';
+
+describe('userData', function () {
+
+    var mockUserResources;
+
+    beforeEach(module("eventsApp"));
+
+    beforeEach(function () {
+        mockUserResources = sinon.stub({get:function () {}, save:function () {}});
+
+        module(function ($provide) {
+            $provide.value('userResource', mockUserResources);
+        })
+    });
+
+    describe('getData', function () {
+
+        it('should call getresource.get with the user name', inject(function (userData) {
+            userData.getUser('bob');
+
+            expect(mockUserResources.get.args[0][0]).toEqual({userName:'bob'});
+        }));
+
+        it('should return whatever userresource.get returns', inject(function (userData) {
+
+            mockUserResources.get.returns('angular rocks');
+
+            var result = userData.getUser('bob');
+
+            expect(result).toBe('angular rocks');
+        }))
+
+    });
+
+    describe('save', function () {
+
+        it('should call userResource.save with the same parameters', inject(function (userData) {
+            userData.save('some value');
+
+            expect(mockUserResources.save.calledWith('some value')).toBe(true);
+        }))
+    });
+});
