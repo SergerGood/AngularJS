a12d5e8208f1bc5e23278482cdb2a6c5eb5a3bff
SergerGood <serzher@inbox.ru>
2016-07-22 10:14:39 +0300

    test ajax services in angular

--- test/unit/evenDataSpec.js
+++ test/unit/evenDataSpec.js
@@ -1,3 +1,21 @@
-/**
- * Created by Khlebnikov.Sergey on 22.07.2016.
- */
+'use strict';
+
+describe('evenData', function () {
+
+    beforeEach(module("eventsApp"));
+
+    it('should issue a GET request to /data/event/11 when getEvent is called and id is 11 ',
+
+        inject(function (evenData, $httpBackend) {
+
+            $httpBackend.expectGET('/data/event/11');
+            $httpBackend.when('GET', '/data/event/11').respond({});
+            evenData.getEvent(11);
+            $httpBackend.flush();
+
+            $httpBackend.verifyNoOutstandingExpectation();
+            $httpBackend.verifyNoOutstandingRequest();
+        })
+
+    );
+});
