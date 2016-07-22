ae7ce373f5821f1c1cbfacfb3636e17ec04bcbfb
SergerGood <serzher@inbox.ru>
2016-07-22 10:47:35 +0300

    test ajax services in angular Part2

--- test/unit/evenDataSpec.js
+++ test/unit/evenDataSpec.js
@@ -16,6 +16,43 @@ describe('evenData', function () {
             $httpBackend.verifyNoOutstandingExpectation();
             $httpBackend.verifyNoOutstandingRequest();
         })
-
     );
+
+    it('should return the correct data when getEvent is called',
+
+        inject(function (evenData, $httpBackend) {
+
+            $httpBackend.when('GET', '/data/event/11').respond({name:'My Event'});
+            var event = evenData.getEvent(11);
+            $httpBackend.flush();
+
+            expect(event.name).toBe('My Event');
+        })
+    );
+
+    it('should issue a GET request to /data/event when getAllEvents is called',
+
+        inject(function (evenData, $httpBackend) {
+
+            $httpBackend.when('GET', '/data/event').respond([{name: 'My Event'}]);
+            var events = evenData.getAllEvents();
+            $httpBackend.flush();
+
+            expect(events[0].name).toBe('My Event');
+        })
+    );
+
+    it('should set the id to 999 when save is calles',
+
+        inject(function(evenData, $httpBackend){
+
+            $httpBackend.when('POST', 'data/event/999').respond({});
+            var event = {name:'My Event'};
+            evenData.save(event);
+            $httpBackend.flush();
+
+            expect(event.id).toBe(999);
+        })
+    );
+
 });
